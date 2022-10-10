d469cefa7dc1c837b95174edb2a52b345e6f225a
Petr <petr.shabunin@ural.rt.ru>
2022-10-10 22:32:25 +0500

    add randomData and faker v2

--- src/test/java/work/home/tests/utils/RandomUtils.java
+++ src/test/java/work/home/tests/utils/RandomUtils.java
@@ -1,7 +1,5 @@
 package work.home.tests.utils;
 
-import com.github.javafaker.Faker;
-
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -24,5 +22,29 @@ public class RandomUtils {
     }
     public static String getRandomPhone(){
         return getRandomLong(9000000000L, 9999999999L).toString();
+    }
+    public static String getRandomDay(){
+        return getRandomLong(1L, 28L).toString();
+    }
+    public static String getRandomYear(){
+        return getRandomLong(1900L, 2020L).toString();
+    }
+    public enum Month {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December;
+        public static Month getRandomMonth() {
+            int monthPick = new Random().nextInt(Month.values().length);
+            return Month.values()[monthPick];
+        }
     }
 }
