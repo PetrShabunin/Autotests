3540f98f4eaafdb9f836ea650313b7a13a86cee3
Petr <petr.shabunin@ural.rt.ru>
2022-10-30 14:35:53 +0500

    add json tests

--- src/test/java/work/home/tests/JsonTest.java
+++ src/test/java/work/home/tests/JsonTest.java
@@ -24,13 +24,14 @@ public class JsonTest {
     }
     //Jackson библиотека
     @Test
-    void jsonTestWithJackson1() throws Exception {
+    void jsonTestWithJackson() throws Exception {
         InputStream inputStream = cl.getResourceAsStream("getProfile.json");
         ObjectMapper objectMapper = new ObjectMapper();
         //objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         GetProfileJsonReader.GetProfileBody getProfileBody = objectMapper.readValue(inputStream, GetProfileJsonReader.GetProfileBody.class);
         assertThat(GetProfileJsonReader.GetProfileBody.getId()).isEqualTo(4212314);
         assertThat(GetProfileJsonReader.GetProfileBody.getLogin()).isEqualTo("test");
+        assertThat(GetProfileJsonReader.UserInfo.getName()).isEqualTo("Тест");
     }
 }
 
