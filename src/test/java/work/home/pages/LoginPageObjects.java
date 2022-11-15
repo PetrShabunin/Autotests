31c1c9dc8314c90be1eb5f6ee585d6110f1bdead
Petr <petr.shabunin@ural.rt.ru>
2022-11-15 21:18:20 +0500

    add jenkins and allure

--- src/test/java/work/home/pages/LoginPageObjects.java
+++ src/test/java/work/home/pages/LoginPageObjects.java
@@ -2,6 +2,8 @@ package work.home.pages;
 
 import com.codeborne.selenide.Condition;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class LoginPageObjects {
@@ -12,14 +14,14 @@ public class LoginPageObjects {
     }
     public LoginPageObjects LoginToCabinet(String name, String pass){
         open("https://lk.rt.ru");
-        $("#t-btn-tab-login").shouldBe(Condition.visible).click();
+        $("#t-btn-tab-login").shouldBe(Condition.visible, Duration.ofSeconds(10)).click();
         $("#username").setValue(name);
         $("#password").setValue(pass);
         $("#kc-login").click();
         return this;
     }
     public LoginPageObjects openChat(){
-        $("#widget_bar").shouldBe(Condition.visible).click();
+        $("#widget_bar").shouldBe(Condition.visible, Duration.ofSeconds(10)).click();
         return this;
     }
 }
