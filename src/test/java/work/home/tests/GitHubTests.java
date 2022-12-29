5158656c0c6dec3e25f04e877c0ffa66d706f717
Petr <petr.shabunin@ural.rt.ru>
2022-12-29 13:50:21 +0500

    Selenide Search

--- src/test/java/work/home/tests/GitHubTests.java
+++ src/test/java/work/home/tests/GitHubTests.java
@@ -1,8 +1,12 @@
 package work.home.tests;
 
+import com.codeborne.pdftest.assertj.Assertions;
 import com.codeborne.selenide.Condition;
+import dev.failsafe.internal.util.Assert;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.interactable;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -13,7 +17,8 @@ public class GitHubTests {
         $("[data-scoped-placeholder=Search]").setValue("selenide").pressEnter();
         $$(".repo-list-item").first().$("a").click();
         $("#wiki-tab").click();
-        $(byText("Soft assertions")).click();
+        $("#wiki-pages-filter").setValue("soft");
+        $(byText("SoftAssertions")).click();
         $(".markdown-body").shouldHave(Condition.text("com.codeborne.selenide.junit5.SoftAssertsExtension"));
     }
 }
